a65ab484737af8d99a04a0a8389254524cf98fcc
dnyaneshwarpanzade77 <dnyaneshwarpanzade77@example.com>
2026-02-17 03:15:06 +0530

    Add Javascript concepts and assignment files

---
+++ Chapter_02_java_concepts/05_core_comments_JS.js
@@ -0,0 +1,11 @@
+//This is Comment.this code will not execute.
+console.log("hello danny!");
+/**
+ *This is a multi line comment. 
+ *This code will not execute.
+ *Date:14-02-2026
+ *Author: Danny
+**/
+
+/* same work for 
+*/
---
+++ Chapter_02_java_concepts/06_core_identifier_JS.js
@@ -0,0 +1,41 @@
+//identifier,literal,operator
+var a=10;
+a=20;
+console.log(a);
+
+//identifier- a is an identifier which is used to store value
+//literal- 10,20,hello,null,true,false,undefined are
+// literals which are used to assign value to variable
+//operator- = is an operator which is used to assign value to variable
+
+//Rules of identifier:
+    //1. It should not be a keyword
+    //2. It should not start with a number
+    //3. It should not contain space
+    //4. It should not contain special characters except _ and $
+    //5. It should be meaningful    
+    //6. must start with a letter, _ or $
+    //7. It is case sensitive   
+
+    var name="danny";
+    console.log(name);
+
+    var $name="$danny";
+    console.log($name); 
+
+    var _name="_danny";
+    console.log(_name);     
+
+    var name1="danny1";
+    console.log(name1);     
+
+    var name_1="danny_1";
+    console.log(name_1);    
+
+    var name$1="danny$1";
+    console.log(name$1);    
+
+    var name1$="danny1$";
+    console.log(name1$);    
+
+    
