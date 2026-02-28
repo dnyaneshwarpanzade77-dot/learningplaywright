8846bd87daaf026292fca365915cfe8bbeb69e37
dnyaneshwarpanzade77 <dnyaneshwarpanzade77@example.com>
2026-02-28 08:12:54 +0530

    Save local changes

---
+++ Chapter_06_switch_statments.js/52_test_question.js
@@ -0,0 +1,40 @@
+// console.log(typeof undefined);
+// console.log(typeof NaN);
+
+//console.log(x);
+//console.log(true + true);
+// console.log(0 || 'hello');
+// console.log(0 ?? 'hello');
+// console.log(10 % 3);
+// const arr = [1, 2, 3]; 
+// arr.push(4); 
+// console.log(arr); 
+
+// let a = 5; 
+// let b = a++; 
+// console.log(a, b);
+
+//if ('') { console.log('yes'); } else { console.log('no'); }
+//console.log(Boolean([]));
+
+//  let status = 200; 
+//  if (status === 200) 
+//     { console.log("OK"); } 
+//  else if (status === 404) 
+//     { console.log("Not Found");
+
+//      } 
+//  else { console.log("Other"); 
+
+//  } 
+ 
+// let day = 3; 
+// switch (day) { 
+//     case 1: console.log("Mon"); 
+//     case 2: console.log("Tue"); 
+//     case 3: console.log("Wed"); 
+//     case 4: console.log("Thu"); 
+//     default: console.log("Other");
+// }
+
+
