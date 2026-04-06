bc00ce7b8eff898403181ff12ac0cd60894ae963
dnyaneshwarpanzade77 <dnyaneshwarpanzade77@example.com>
2026-04-06 21:49:27 +0530

    chore: commit all workspace changes

---
+++ Chapter_10_strings/103_String_Properties.js
@@ -0,0 +1,15 @@
+//String properties and basic access
+let str = "Hello World";
+//length start from 1 
+console.log(str.length);
+
+//Access by index
+console.log(str[0]);
+console.log(str[6]);
+console.log(str.at(-1));
+console.log(str.at(-5));
+
+// charAt()
+str.charAt(0);   // "H"
+// charCodeAt() — Unicode value
+str.charCodeAt(0);  // 72
